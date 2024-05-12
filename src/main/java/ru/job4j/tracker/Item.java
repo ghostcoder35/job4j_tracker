ac9dd91f249bdc6701ed2310a7c3ac7efaf00f22
Sergey <graff.MS@rambler.ru>
2024-05-12 10:30:53 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -46,7 +46,7 @@ public class Item {
         return "Item{"
                 + "id=" + id
                 + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
+                + ", created = " + created.format(FORMATTER)
                 + '}';
     }
 }
